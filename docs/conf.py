653125df82a5eee869ad76339b368f8c47c9a352
Mike McIntire <Mike.McIntire@mikes-mbp.lan>
2023-06-08 16:00:43 -0500

    update yml action file and config.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,36 +1,82 @@
 # Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
+# Need this so sphinx can find lumache.py. Change is .py files are elsewhere than root.
+sys.path.insert(0, os.path.abspath('../..'))
+sys.path.insert(0, os.path.abspath('../../lyra_graphtool'))
+
+# -- Project information
 
 project = 'Lyra Graphtool'
-copyright = '2023, Xeek'
+copyright = '2023'
 author = 'Xeek'
-release = '0.0.1'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+release = '0.1'
+version = '0.0.1'
+
+# -- General configuration
 
 extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.napoleon'
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'autoapi.extension',
+    #'sphinxcontrib.apidoc'
 ]
 
+autoapi_type = 'python'
+autoapi_dirs = ['../../lyra_graphtool']
+
+#apidoc_module_dir = '../../src'
+#apidoc_output_dir = '.'
+#apidoc_excluded_paths = ['tests']
+#apidoc_separate_modules = True
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
+html_context = {
+    "display_github": True, # Integrate GitHub
+    "github_repo": "studio-x-llc/lyra_graphtool", # Repo name
+    "github_version": "main", # Version
+    "conf_py_path": "./docs/", # Path in the checkout to the docs root
+}
+
+# -- Options for HTML output -------------------------------------------------
+
+html_short_title = "lyra_graphtool"
+html_show_sourcelink = False
+html_show_sphinx = True
+html_show_copyright = True
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+repository_url = f"https://github.com/studio-x-llc/lyra_graphtool.git"
+html_context = {
+    "menu_links": [
+        (
+            '<i class="fa fa-github fa-fw"></i> Source Code',
+            repository_url,
+        ),
+        (
+            '<i class="fa fa-book fa-fw"></i> License',
+            f"{repository_url}/blob/main/LICENSE",
+        ),
+    ],
+}
